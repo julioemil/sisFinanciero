438570bb58cf4c13f36e33ce98c9494994298cf1
JULIO EMIL <julio.emil.huaman@gmail.com>
2018-02-24 22:31:14 -0500

    AVANCE DETALLE CLIENTE
    AVANCE DETALLE CLIENTE

--- application/views/view_listado_cliente.php
+++ application/views/view_listado_cliente.php
@@ -38,7 +38,7 @@ $usuario = $this->model_usuarios->BuscarID($cliente->idUsuario); foreach($usuari
 <th>TELEFONO 1:</th>
 <td><?php echo $cliente->telefono;?></td>
 <th>TELEFONO 2:</th>
-<td></td>
+<td><?php echo $cliente->telefono2;?></td>
 <th>CORREO:</th>
 <td><?php echo $cliente->email;?></td>
 </tr>
@@ -57,24 +57,50 @@ $usuario = $this->model_usuarios->BuscarID($cliente->idUsuario); foreach($usuari
     <td colspan="6"></td>
 </tr>
 <tr>
-<th>DIRECCIÓN:</th>
-<td><?php echo $cliente->direccion;?></td>
+    <th>ESTADO:</th>
+    <td colspan="6"><?php echo $cliente->estado;?></td>
+</tr>
+<tr>
+    <td colspan="6"><hr/></td>
+</tr>
+<tr>
+    <td colspan="6"><h5>DATOS DE DIRECCIÓN DE DOMICILIO:</h5></td>
+</tr>
+<tr>
+<th>DEPARTAMENTO:</th>
+<td><?php echo $cliente->departamentoNegocio;?></td>
+<th>PROVINCIA:</th>
+<td><?php echo $cliente->provinciaNegocio;?></td>
+<th>DISTRITO:</th>
+<td><?php echo $cliente->distritoNegocio; ?></td>
 </tr>
 <tr>
     <td colspan="6"></td>
 </tr>
 <tr>
-<th>DEPARTAMENTO:</th>
-<td><?php echo $cliente->departamento;?></td>
-<th>PROVINCIA:</th>
-<td><?php echo $cliente->provincia;?>
-</td>
-<th>DISTRITO:</th>
-<td><?php echo $cliente->distrito; ?>
-</td>
+<th>DIRECCIÓN:</th>
+<td colspan="6"><?php echo $cliente->direccionNegocio;?></td>
 </tr>
 <tr>
     <td colspan=6><hr/></td>
+</tr>
+<tr>
+    <td colspan="6"><h5>DATOS DE DIRECCIÓN DE NEGOCIO:</h5></td>
+</tr>
+<tr>
+<th>DEPARTAMENTO:</th>
+<td><?php echo $cliente->departamento;?></td>
+<th>PROVINCIA:</th>
+<td><?php echo $cliente->provincia;?></td>
+<th>DISTRITO:</th>
+<td><?php echo $cliente->distrito; ?></td>
+</tr>
+<tr>
+    <td colspan="6"></td>
+</tr>
+<tr>
+<th>DIRECCIÓN:</th>
+<td colspan="6"><?php echo $cliente->direccion;?></td>
 </tr>
 <tr>
     <td colspan="3" align="center">   
